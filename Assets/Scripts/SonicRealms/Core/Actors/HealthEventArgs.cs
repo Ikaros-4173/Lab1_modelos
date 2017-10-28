2499fd5842e0a8aea5e30f1c1dbfc45504d1ac21
Ikaros-4173 <alexanderecheverra84@gmail.com>
2017-10-28 16:28:26 -0500

    Subo mis avances

---
+++ Assets/Scripts/SonicRealms/Core/Actors/HealthEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace SonicRealms.Core.Actors
+{
+    /// <summary>
+    /// Some data to pass in when a health-related event occurs.
+    /// </summary>
+    [Serializable]
+    public class HealthEventArgs
+    {
+        public float Damage;
+        public Transform Source;
+    }
+}
